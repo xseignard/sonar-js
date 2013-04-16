916c82855214cf1625c798885d4bd99dca5f3c41
xseignard <xavier.seignard@gmail.com>
2013-04-17 00:46:11 +0200

    added one test + test directory in sonar-project.properties

--- test/core/calculator.test.js
+++ test/core/calculator.test.js
@@ -11,5 +11,10 @@ describe('Calculator', function() {
 				result = calculator.add(2,3);
 			assert.equal(result, 5);
 		});
+		it('should make an addition with negative numbers', function() {
+			var calculator = new Calculator(),
+				result = calculator.add(2,-3);
+			assert.equal(result, -1);
+		});
 	});
 });
